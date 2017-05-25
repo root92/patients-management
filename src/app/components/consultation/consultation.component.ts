90cbdab288e7f980bb6851e1e84bff495aa0b2f5
BALDE Thierno Lamine <tlbbalde92@gmail.com>
2017-05-25 15:35:15 +0200

    Add consulation component and breadcrumb

---
+++ src/app/components/consultation/consultation.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
+
+@Component({
+  selector: 'app-consultation',
+  templateUrl: './consultation.component.html',
+  styleUrls: ['./consultation.component.css']
+})
+export class ConsultationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
